afb9ea051eea94b0d8d85377ee353f1d847cf744
RS <git@rolandsiebelink.com>
2011-07-16 13:40:01 -0700

    Lots of changes, "idle" automatically created when not existing.

--- chaseidle.rb
+++ chaseidle.rb
@@ -2,38 +2,30 @@
 
 require '/Users/rs/Dropbox/Library/Scripts/Routinetracker/_routine_methods.rb'
 
-if File.exist?("/Users/rs/Desktop/silent") then
-  stil = true
-else
-  stil = false
+class Main
+  begin
+    f = File.open("/tmp/routinetracker.log", "r")
+    line = f.gets
+    f.close
+  rescue
+    f = File.new("/tmp/routinetracker.log", "w+")
+    f.puts("IDLE man!")
+    retry
+  end
+
+  #puts "The string is $#{line}$"
+  testwaarde = line.match("IDLE").to_s
+  if testwaarde == "IDLE" then
+#    system("/usr/local/bin/growlnotify -p 4 -m 'IDLE' ")    
+    system("/usr/local/bin/growlnotify -p 5 #{line} ")    
+    unless File.exist?("/Users/rs/Desktop/silent") then
+      system("say #{line}")
+    end
+  else
+    #firstword = line.split(/ /)[0]
+    #system("/usr/local/bin/growlnotify -p 0 -m #{firstword} ")    
+    system("/usr/local/bin/growlnotify -p 0 -m #{line} ")    
+  end
+
 end
 
-File.open("/tmp/routinetracker.log", "r")  do |f|
-  while (line = f.gets)
-    # puts "The string is $#{line}$"
-    testwaarde = line.match("IDLE").to_s
-    if testwaarde == "IDLE" then
-      system("/usr/local/bin/growlnotify -p 0 -m \"stijvelul IDLE\" ")
-      unless stil 
-        say("IDLE")
-      end
-    else
-      tijdmelding = Time.now().strftime("%H:%M:%S")
-      system("/usr/local/bin/growlnotify -p 0 -t \"lekkerkontje #{tijdmelding}\" -m \"#{line}\" ")
-      firstword = line.split(/ /)[0]
-      unless stil 
-        say(firstword)
-      end
-    end
-  end
-  f.close
-end
-# 
-# 
-# 
-# file = File.new("readfile.rb", "r")
-#    9:while (line = file.gets)
-#   10:    puts "#{counter}: #{line}"
-#   11:    counter = counter + 1
-#   12:end
-#   13:file.close
