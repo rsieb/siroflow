fb128f1f294b63d80442cb3394242b72a8b45c23
RS <git@rolandsiebelink.com>
2011-10-26 13:01:42 -0700

    Deleting routinetracker2.rb--have to refactor in same file I think

--- sandbox.rb
+++ sandbox.rb
@@ -1,13 +1,19 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require 'appscript'
-require 'osax'
-require '_routine_methods'
+require 'appscript' ; include Appscript
 
-doel = 1837 # seconden
-# rare omweg om met Time te kunnen werken
-#  t = Time.gm(2000,"jan",1,20,15,1)   #=> Sat Jan 01 20:15:01 UTC 2000
-puts doel, doel.to_human, doel.to_minuteur
-# include Appscript
-#  app("Minuteur").StartCountdown("001000")
+
+
+# doel = 1837 # seconden
+# # rare omweg om met Time te kunnen werken
+# #  t = Time.gm(2000,"jan",1,20,15,1)   #=> Sat Jan 01 20:15:01 UTC 2000
+# puts doel, doel.to_human, doel.to_minuteur
+# # include Appscript
+# #  app("Minuteur").StartCountdown("001000")
+
+begin
+  app("Pomodoro").reset
+rescue
+  app("Pomodoro").start
+end  
