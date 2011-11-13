8e3bfc847c0ad37c57183ef19fd8b74d8e985e7f
RS <git@rolandsiebelink.com>
2011-11-12 16:23:36 -0800

    Remodeling chaseidle in classes

--- chaseidle.rb
+++ chaseidle.rb
@@ -1,49 +1,117 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby -wKU
+# encoding: UTF-8
 
+require 'rubygems'
+require 'appscript'
 require '/Users/rs/Dropbox/Library/Scripts/Routinetracker/_routine_methods.rb'
 
-class Main
-  begin
-    f = File.open("/tmp/routinetracker.log", "r")
-    line = f.gets
-    f.close
-  rescue
-    f = File.new("/tmp/routinetracker.log", "w+")
-    f.write("IDLE •")
-    retry
-  end
 
-  #puts "The string is $#{line}$"
-  testwaarde = line.match("IDLE").to_s
-  puts line
-  puts testwaarde
+module IdleChaser
+  ACIVITYFILE = "/tmp/routinetracker.log"
 
-  if testwaarde == "IDLE" then
-    #    system("/usr/local/bin/growlnotify -p 4 -m 'IDLE' ")    
-    system("/usr/local/bin/growlnotify -p 5 -t 'Superfocus' -m '#{line}' ")    
-    sleep 2
-    system("open -a ScreenSaverEngine")
-    f = File.open("/tmp/routinetracker.log", "a")
-    f.write("•")
-    f.close
-    unless File.exist?("/Users/rs/Desktop/silent") then
-      system("say #{line}")
+  class Terminal
+
+    def initialize
     end
-  # else
-  #   # DONE 201111031354 temporary implementation before changing routinetracker
-  #   # sayable2 = line.split(/\#/)[0]
-  #   # sayable = sayable2.split("<")[0] + " by " + sayable2.split("--")[1]
-  #   # DONE 201111031354 replace this over time with 
-  #   sayable = line.split(" ")[0]
-  #   tijd = line.split(" by ")[1]
-  #   unless !tijd
-  #     sayable = "#{sayable} by #{tijd}"
-  #   end
-  #   system("/usr/local/bin/growlnotify -p 0 -t 'Superfocus' -m '#{line}' ")    
-  #   unless File.exist?("/Users/rs/Desktop/silent") then
-  #     system("say '#{sayable}'")
-  #   end
+
+    @@instance = Terminal.new
+
+    def self.instance
+      return @@instance
+    end
+
+    def self.silent?
+      @@silent File.exist?("/Users/rs/Desktop/silent")
+    end
+
+    def self.say(saying)
+      system("/usr/local/bin/growlnotify -p 0 -m \"#{saying}\" ")
+      unless self.silent
+        system("say #{saying}")
+      end
+    end
+
+    def self.chaseup(sayable)
+      say(sayable)
+      sleep 2
+      system("open -a ScreenSaverEngine")
+      f = File.open("/tmp/routinetracker.log", "a")
+      f.write("*")
+      f.close
+    end
+
+    def self.remind(sayable)
+      say(sayable)
+    end
+
+    private_class_method :new
   end
 
-end
+  class Planning
+    def initialize
+      @events = Appscript::app("iCal").calendars["Planning"].events.get
+    end
 
+    def target
+      currentappt = @events.find_all{|mtg|
+        mtg.start_date.get < Time.now() &&
+        mtg.end_date.get > Time.now()
+        }.first.summary.get
+        if ( !currentappt || @current == :missing_value )
+          return nil
+        else
+          return currentappt
+        end
+      end
+
+      def actual
+        begin 
+          f = File.open("/tmp/routinetracker.log", "r")
+          line = f.gets
+        rescue
+          # if this file doesn't exist, we just create it
+          f = File.new("/tmp/routinetracker.log", "w+")
+          f.write("IDLE *")
+          retry
+        ensure
+          f.close
+        end
+
+        return line
+      end
+
+      def idle?(input)
+        input.match("IDLE").to_s
+      end
+
+    end
+
+    class Main
+
+      if testwaarde == "IDLE" then
+
+        #    system("/usr/local/bin/growlnotify -p 4 -m 'IDLE' ")    
+        system("/usr/local/bin/growlnotify -p 5 -t 'Superfocus' -m '#{line}' ")    
+        unless File.exist?("/Users/rs/Desktop/silent") then
+          system("say '#{sayable}'")
+        end
+        #    load '/Users/rs/Dropbox/Library/Scripts/Routinetracker/plannedpomodoro.rb'
+      else
+        # DONE 201111031354 temporary implementation before changing routinetracker
+        # sayable2 = line.split(/\#/)[0]
+        # sayable = sayable2.split("<")[0] + " by " + sayable2.split("--")[1]
+        # DONE 201111031354 replace this over time with 
+        sayable = line.split(" ")[0]
+        # tijd = line.split(" by ")[1]
+        # unless !tijd
+        #   sayable = "#{sayable} by #{tijd}"
+        # end
+        system("/usr/local/bin/growlnotify -p 0 -t 'Superfocus' -m '#{line}' ")    
+        unless File.exist?("/Users/rs/Desktop/silent") then
+          system("say '#{sayable}'")
+        end
+      end
+
+    end
+  end
+end
