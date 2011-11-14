6d4212ea4013fd56b1768dd32b2254b6ef9a5d44
RS <git@rolandsiebelink.com>
2011-11-13 22:29:02 -0800

    Added routrack rails application

---
+++ routrack/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Routrack::Application.config.secret_token = 'd0a09d66a836df4817058ec4d953f735aa89805276094baa860878bb6e6fb0734edc4d246b0443109236ba456d610a2a7240b91714ad9804822fb7e41dce371a'
