31a3563924347039153f9a293ac801ceedd2cb95
RS <git@rolandsiebelink.com>
2012-06-30 16:01:31 -0700

    Caught Error in ChaseIdle when no tasks starred in ToodleDo

--- Activity.rb
+++ Activity.rb
@@ -4,10 +4,22 @@ module RoutineTracker
     def self.target
       tasks = Array.new()
       nowtask = ""
-      tasks.push(%x[/Users/rs/Dropbox/Library/Scripts/geektool/currentevents.sh].chomp!)
-      tasks.push(IO.readlines("/Users/rs/Dropbox/Library/Scripts/geektool/starredtasks.txt").last.chop.to_s)
+      begin
+        tasks.push(%x[/Users/rs/Dropbox/Library/Scripts/geektool/currentevents.sh].chomp!)
+      rescue
+        tasks.push("--No active event or ical error--")
+        tasks.push("Fuck yeah restar my tasklist ®restar +0pvy")
+      end
+      begin
+        tasks.push(IO.readlines("/Users/rs/Dropbox/Library/Scripts/geektool/starredtasks.txt").last.chop.to_s)
+      rescue
+        tasks.push("--No starred tasks or Toodledo error--")
+      end
       tasks.size.times do
-        nowtask = nowtask + tasks.delete_at(rand(tasks.size)) + "\n"
+        addtask = tasks.delete_at(rand(tasks.size))
+        if addtask then
+          nowtask = nowtask + addtask  + "\n"
+        end
       end
       return nowtask
     end
