405eafe802a2d1b26d294be5e30b72326dd79388
RS <git@rolandsiebelink.com>
2012-07-08 10:05:24 -0700

    Checking in at start

---
+++ add_multiple_pomodori.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+#require 'yaml'
+#require 'appscript' ; include Appscript
+# #require 'osax' ; include OSAX
+require 'readline'
+#require 'active_support/core_ext/integer/inflections'
+#require_relative 'Terminal'
+#ψ Most small routines in separate file
+#require './_routine_methods'
+# This line is causing errors af
+#require_relative 'routrack/config/application'
+
