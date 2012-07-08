3ead0b8483610f8709d73d8e6b7ff77d689094a1
RS <git@rolandsiebelink.com>
2012-07-08 10:50:40 -0700

    Version should be working. Now let's get a move on with real work!

--- add_multiple_pomodori.rb
+++ add_multiple_pomodori.rb
@@ -1,14 +1,34 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
-#require 'yaml'
-#require 'appscript' ; include Appscript
-# #require 'osax' ; include OSAX
-require 'readline'
-#require 'active_support/core_ext/integer/inflections'
-#require_relative 'Terminal'
-#ψ Most small routines in separate file
-#require './_routine_methods'
-# This line is causing errors af
-#require_relative 'routrack/config/application'
+TOODLEDO = "/Users/rs/.rvm/gems/ruby-1.9.3-p194/bin/toodledo"
+require "rubygems"
+require "readline"
 
+@task = ""
+@goal = ""
+@number = 0
+@confirm = nil
+
+
+while @task.size <= 10
+  @task = Readline.readline("What is your reddest MBO task? \n> ",true).chomp
+end
+while @goal.size != 4
+  @goal = Readline.readline("What is the Toodledo goal for `#{@task}`?\n" +'[....]> ',true).chomp
+end
+while @number < 2
+  @number = Readline.readline("How many Pomodori would you like to add for `#{@task}`?\n[2-]> ",true).chomp.to_i
+end
+while @confirm == nil
+  @confirm = Readline.readline("Add #{@number} Pomodori for `#{@task}` with goal `#{@goal}`?\n[y][N]\n> ",true)
+end
+if @confirm == "y"
+  @number.times do |teller|
+    # ttodo 2012-07-08 investigate why single asterisk is not working for starring a task. Code issue?
+    emailinput = "#{@task} #{@number - teller}/#{@number} * ^#{@goal} !High" 
+    puts emailinput
+    system("#{TOODLEDO} add '#{emailinput} '")
+  end
+else
+  puts "Canceled"
+end
