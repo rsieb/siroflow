99cabdd27f034f5c423c9e9ff4d69d0a8075dc92
Roland Siebelink <roland+github@rolandsiebelink.com>
2013-03-31 22:59:36 -0700

    This seems not to have been in Git already... About time...

--- pull_active_from_pivotal.rb
+++ pull_active_from_pivotal.rb
@@ -5,10 +5,15 @@ require 'active_resource'
 require 'pp'
 require 'pivotal-tracker'
 
+def initials(stringetje)
+  lengte = (stringetje.size - 2)
+  return stringetje[0]+lengte.to_s+stringetje[-1]
+end
+
 if ARGV[0] == "--all" then
-	@mystate = ["started","finished","rejected","unestimated","unstarted"]
+  @mystate = ["started","finished","rejected","unestimated","unstarted"]
 else
-	@mystate = ["started","rejected","unestimated"]
+  @mystate = ["started","rejected","unestimated"]
 end
 
 
@@ -18,40 +23,40 @@ PivotalTracker::Client.token('roland@rocketfuelinc.com', 'qub0y?Qatar')        #
 
 @myprojects = [780227,781813,786005] # ,479975
 @myprojects.each do |projectnummer|
-	@a_project = PivotalTracker::Project.find(projectnummer) 
-	@mystories = @mystories + @a_project.stories.all(:state => @mystate)
+  @a_project = PivotalTracker::Project.find(projectnummer)
+  @mystories = @mystories + @a_project.stories.all(:state => @mystate)
 end
 
 @mystories.each do |verhaaltje|
-	#pp verhaaltje
-	begin
-		mijnnaam   = verhaaltje.name
-	rescue
-		mijnnaam   = "!NoStory"
-	end
-	begin
-		eigenaar   = verhaaltje.owned_by
-	rescue
-		eigenaar   = "!NoOwner"
-	end
-	begin
-		mijntaak = false
-		verhaaltje.tasks.all.each do |taakje|
-				if !taakje.complete and !mijntaak
-					mijntaak = taakje.description
-				end
-		end
-	rescue
-		mijntaak   = "!NoTask"
-	end
-	begin
-		mijnetiket = verhaaltje.labels
-	rescue
-		mijnetiket = "!NoLabel"
-	end
-	if eigenaar == "Roland Siebelink"
-		puts "#{mijntaak}<#{mijnnaam} +#{mijnetiket}"
-	else 
-		puts "[#{eigenaar}] #{mijntaak}<#{mijnnaam} +#{mijnetiket}"
-	end
+  #pp verhaaltje
+  begin
+    mijnnaam   = verhaaltje.name
+  rescue
+    mijnnaam   = "!NoStory"
+  end
+  begin
+    eigenaar   = verhaaltje.owned_by
+  rescue
+    eigenaar   = "!NoOwner"
+  end
+  begin
+    mijntaak = "!NoTask"
+    verhaaltje.tasks.all.each do |taakje|
+      if !taakje.complete and mijntaak == "!NoTask"
+        mijntaak = taakje.description
+      end
+    end
+  rescue
+    mijntaak   = "!NoTask"
+  end
+  begin
+    mijnetiket = verhaaltje.labels
+  rescue
+    mijnetiket = "!NoLabel"
+  end
+  if eigenaar=="Roland Siebelink" then 
+  	puts "#{mijnnaam}>>#{mijntaak} +#{mijnetiket}"
+  else
+    puts "•#{initials(eigenaar)} #{mijnnaam} +#{mijnetiket}"
+  end
 end
