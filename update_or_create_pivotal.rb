01e7ce4268de256b5e9d205f67c2e5c6e5717a01
Roland Siebelink <git@rolandsiebelink.com>
2013-07-01 22:04:20 -0700

    Automatically putting stories on finished when I have worked on them for one Pomodoro--i.e. restart manually when not done

--- update_or_create_pivotal.rb
+++ update_or_create_pivotal.rb
@@ -12,16 +12,16 @@ PivotalTracker::Client.token('roland@rocketfuelinc.com', 'qub0y?Qatar')        #
 @pomodoro_name = ARGV[0]
 ## skip for routines
 if @pomodoro_name.include?("+rout") then
-	puts "Routine task. Aborted."
-	abort
+  puts "Routine task. Aborted."
+  abort
 end
 
 if @pomodoro_name.include?("+beem") then
-	myslug = @pomodoro_name.split[1] # second word from array
-	puts "Beeminder task. Opening URL with slug `#{myslug}'"
-	myurl = "http://beeminder.com/cyberroland/goals/#{myslug}"
-	system("/usr/bin/osascript -e 'open location  \"#{myurl}\" ' ") 
-	abort
+  myslug = @pomodoro_name[/\w+ \+beem/][/\w+/] # user to put slug as last word before +beem code
+  puts "Beeminder task. Opening URL with slug `#{myslug}'"
+  myurl = "http://beeminder.com/cyberroland/goals/#{myslug}"
+  system("/usr/bin/osascript -e 'open location  \"#{myurl}\" ' ")
+  abort
 end
 
 # TODO 2013-04-30 Catch Beeminder routines to open corresponding goal in Beeminder for updating
@@ -31,42 +31,46 @@ end
 ## Cycle through projects
 @myprojects = [781813] # ,780227,,786005] #,787023] # ,479975
 @myprojects.each do |projectnummer|
-	# some spring cleaning needed first!
-	# TODO 2013-05-22 why don't we make the mess below into a proper regex :)
-	@basepomname = @pomodoro_name.sub("√","").sub(/>>.*/,"")
-	#@nametocheck = @basepomname[0]+@basepomname[2..-1] 
-	@nametocheck = @basepomname[2..-1] # does not need to capture the priority level, is immaterial
-	###puts projectnummer
+  # some spring cleaning needed first!
+  # TODO 2013-05-22 why don't we make the mess below into a proper regex :)
+  @basepomname = @pomodoro_name.sub("√","").sub(/>>.*/,"")
+  #@nametocheck = @basepomname[0]+@basepomname[2..-1]
+  @nametocheck = @basepomname[2..-1] # does not need to capture the priority level, is immaterial
+  ###puts projectnummer
 
-	@a_project = PivotalTracker::Project.find(projectnummer) 
-	## -- find the story with this title
-	@allstories = @a_project.stories.all()
-	@allstories.each do |verhaaltje|
-		if verhaaltje.name.include?(@nametocheck) || @nametocheck.include?(verhaaltje.name) then
-			@mystories.push(verhaaltje)
-			###puts "Adding #{verhaaltje.id} to @mystories"
-		end # if include
-	end # stories cycle	
+  @a_project = PivotalTracker::Project.find(projectnummer)
+  ## -- find the story with this title
+  @allstories = @a_project.stories.all()
+  @allstories.each do |verhaaltje|
+    if verhaaltje.name.include?(@nametocheck) || @nametocheck.include?(verhaaltje.name) then
+      @mystories.push(verhaaltje)
+      ###puts "Adding #{verhaaltje.id} to @mystories"
+    end # if include
+  end # stories cycle
 end # projects cycle
 
 ## -- found? yes then
 if @mystories.size > 0 then
-	pp "Found one"
-	## I don't really want to create automated comments!
-	@mystories.each do |verhaaltje|
-		## Just open the found story in the UI
-		system("/usr/bin/osascript -e 'open location  \"" + verhaaltje.url + "\" ' ")
-	end
-## -- if not found then
-#else
-# 	pp "Did not find one"
-# 	## -- set the default project
-# #	@defaultproject = PivotalTracker::Project.find(786005)  # this is the "management" project
-# 	@defaultproject = PivotalTracker::Project.find(787023)  # this is the "inbox" project
-# 	## ---- create a new story in the default project
-# 	@mynewstory = @defaultproject.stories.create(:name => @nametocheck, :story_type => 'bug')
-# 	pp @mynewstory
-# 	## Just open the found story in the UI
-# 		system("/usr/bin/osascript -e 'open location  \"" + @mynewstory.url + "\" ' ")
-# 	## -- end
+  pp "Found one"
+  ## I don't really want to create automated comments!
+  @mystories.each do |verhaaltje|
+    ## Just open the found story in the UI
+    #if verhaaltje.current_state == "Unstarted"
+    #then verhaaltje.current_state = "Started"
+    verhaaltje.current_state = "Finished"
+    #end
+    system("/usr/bin/osascript -e 'open location  \"" + verhaaltje.url + "\" ' ")
+  end
+  ## -- if not found then
+  #else
+  # 	pp "Did not find one"
+  # 	## -- set the default project
+  # #	@defaultproject = PivotalTracker::Project.find(786005)  # this is the "management" project
+  # 	@defaultproject = PivotalTracker::Project.find(787023)  # this is the "inbox" project
+  # 	## ---- create a new story in the default project
+  # 	@mynewstory = @defaultproject.stories.create(:name => @nametocheck, :story_type => 'bug')
+  # 	pp @mynewstory
+  # 	## Just open the found story in the UI
+  # 		system("/usr/bin/osascript -e 'open location  \"" + @mynewstory.url + "\" ' ")
+  # 	## -- end
 end
