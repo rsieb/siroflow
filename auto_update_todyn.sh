e26d994ba86208bad90913bc3b13d86669fa24c1
Roland Siebelink <git@rolandsiebelink.com>
2013-08-21 11:46:05 -0700

    Not filtering appointments for the day through head-3 anymore, this makes me lose attention

--- auto_update_todyn.sh
+++ auto_update_todyn.sh
@@ -19,28 +19,30 @@ TODOYFILE="$HOMEDIR/Todoy.txt"
 DAYBREAK="$HOMEDIR/Todobreak.txt"
 touch -t `date '+%m%d0001'` $DAYBREAK
 
-if [[ ! -f $TMPFILE || `find $TMPFILE -mmin +5` ]];
+if [[ ! -f $TMPFILE || `find $TMPFILE -mmin +1` ]];
+then
+	touch $TMPFILE # update time stamp so that the script cannot run twice in parallel
+	if [[ `ping -o www.google.com` ]];
 	then
-		touch $TMPFILE # update time stamp so that the script cannot run twice in parallel
-		if [[ `ping -o www.google.com` ]];
-			then
-		
+	
 		# Do we need to update the daily goals first? Is Todoy older than 00:01 today?
-		if [[ ! -f $TODOYFILE || `find $DAYBREAK -newer $TODOYFILE` ]];
-		then
-			. /Users/rs/rt/add_daily_to_todoy.sh
+		if [[ ! -f $TODOYFILE || `find $DAYBREAK -newer $TODOYFILE` ]] ; then
+			if [[ $(date +%H) -gt 3 ]] ; then
+				#say "I would update Beeminder now"
+				. /Users/rs/rt/add_daily_to_todoy.sh
+			fi
 		fi
 
 		# update written and diary goals from computer
-		MYFILE="$HOMEDIR/Beeminder.txt"
+		MYFILE1="$HOMEDIR/Beeminder-redplus.txt"
+		MYFILE2="$HOMEDIR/Beeminder-orange.txt"
 		#echo -e "\n# From Beeminder... #" > "$MYFILE"
-		echo -e "\n" > "$MYFILE"
-		# if [[ $(date +%u) -ne 6 ]] # only 3 on non Saturdays
-		# then
-			. /Users/rs/rt/beeminder-pull-endangered.sh | sort -r | head -3 >> "$MYFILE" 
-		# else
-		# 	. /Users/rs/rt/beeminder-pull-endangered.sh | sort -r | head -6 >> "$MYFILE" 
-		# fi
+			. /Users/rs/rt/beeminder-pull-endangered.sh > /tmp/beeminderoutput.txt 
+		echo -e "\n" > "$MYFILE1"
+			cat /tmp/beeminderoutput.txt | sort -r | grep "UNKNOWN" | head -3 >> "$MYFILE1" 
+			cat /tmp/beeminderoutput.txt | sort -r | grep "RED" | head -3 >> "$MYFILE1" 
+		echo -e "\n" > "$MYFILE2"
+			cat /tmp/beeminderoutput.txt | sort -r | grep "ORANGE" | head -3 >> "$MYFILE2" 
 		#say "Beeminder updated"
 
 		# from Pivotal tracker
@@ -49,7 +51,7 @@ if [[ ! -f $TMPFILE || `find $TMPFILE -mmin +5` ]];
 		echo -e "\n" > "$MYFILE"
 		# if [[ $(date +%u) -ne 6 ]] # no proactive work on Saturdays
 		# then
-			ruby -KT /Users/rs/rt/pull_active_from_pivotal.rb | head -3 >> "$MYFILE" 
+			ruby -KT /Users/rs/rt/pull_active_from_pivotal.rb | head -7 >> "$MYFILE" 
 		# fi
 		#say "Pivotal updated"
 
@@ -64,31 +66,30 @@ if [[ ! -f $TMPFILE || `find $TMPFILE -mmin +5` ]];
 		MYFILE="$HOMEDIR/Mailboxtodo.txt"
 		#echo -e "\n# From Mailbox/ToDo... #" > "$MYFILE"
 		echo -e "\n" > "$MYFILE"
-		ruby -KT /Users/rs/rt/gmailtodo.rb >> "$MYFILE" 
+		ruby -KT /Users/rs/rt/gmailtodo.rb | head -3 >> "$MYFILE" 
 		#say "Mailbox-to-do updated"
 
 		# Pull together Todyn (dynamic todo)
 		cd $HOMEDIR
-		cat Calendar.txt Beeminder.txt Pivotal.txt Mailboxtodo.txt Todoy.txt > $TMPFILE
+		cat Calendar.txt Beeminder-redplus.txt Beeminder-orange.txt Pivotal.txt Mailboxtodo.txt Todoy.txt > $TMPFILE
 		# Mark as today
 		echo "" >> $TMPFILE
 		echo -e "\n`date '+%Y-%m-%d %H:%M'`" >> $TMPFILE 
-
 	fi
-		# Take out ignored lines
-		# subtracting two files from each other as per <http://aijazansari.com/2011/11/23/how-to-subtract-one-file-from-another/>
-		# NB file with negative lines should come first :)
-		if [[ -s $TODONTFILE ]] ; then
-			# first ensure there are no white lines in Todont
-			grep -v '^$' $TODONTFILE > /tmp/Todont.txt
-			cp /tmp/Todont.txt $TODONTFILE
-			# now take all the ignorable lines out of the temporary Todyn by matching against Todont
-			grep --invert-match --line-regexp --file=$TODONTFILE $TMPFILE | cat -s > $TODYNFILE 
-		else
-			cat -s $TMPFILE > $TODYNFILE
-		fi
+	# Take out ignored lines
+	# subtracting two files from each other as per <http://aijazansari.com/2011/11/23/how-to-subtract-one-file-from-another/>
+	# NB file with negative lines should come first :)
+	if [[ -s $TODONTFILE ]] ; then
+		# first ensure there are no white lines in Todont
+		grep -v '^$' $TODONTFILE > /tmp/Todont.txt
+		cp /tmp/Todont.txt $TODONTFILE
+		# now take all the ignorable lines out of the temporary Todyn by matching against Todont
+		grep --invert-match --line-regexp --file=$TODONTFILE $TMPFILE | cat -s > $TODYNFILE 
+	else
+		cat -s $TMPFILE > $TODYNFILE
+	fi
 else
-	echo "Todyn updated less than five minutes ago... Aborting."
+	echo "Todyn updated less than one minute ago... Aborting."
 fi
 #osascript -e 'tell application "nvALT" to search "Todyn" ' -e 'tell application "nvALT" to activate'
 
