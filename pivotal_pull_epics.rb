948346262fab8cd5794c3177a2c83b1b59f76287
Roland Siebelink <git@rolandsiebelink.com>
2013-11-11 18:48:12 -0800

    Also adding these three new files that I may in fact be dependent on

---
+++ pivotal_pull_epics.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby -KU
+
+require 'rubygems'
+require 'bundler/setup'
+#require 'active_resource'
+require 'pp'
+require 'pivotal-tracker'
+
+@mystate = %w( started rejected unstarted delivered finished )
+
+PivotalTracker::Client.token('roland@rocketfuelinc.com', 'qub0y?Qatar')        # Automatically fetch API Token
+
+@mystories = Array.new()
+
+@myprojects = [781813] # take out FUEL 780227,786005,479975
+@myprojects.each do |projectnummer|
+  @a_project = PivotalTracker::Project.find(projectnummer)
+  @mystories = @mystories + @a_project.stories.all(:state => @mystate)
+  #pp @mystories
+end
+
+myepics = Array.new
+
+@mystories.each do |verhaaltje|
+  begin
+    mijnetiket = Array(verhaaltje.labels.split(","))
+  rescue
+    mijnetiket = Array("NO LABEL")
+  end
+  #puts "Mijnetiket = #{mijnetiket}"
+  mijnprioriteit=""
+  begin
+    mijnetiket.each do |etiketje|
+      #puts "etiketje = #{etiketje}"
+      if etiketje[0] =~ /[0-9]/
+        # this is an epic label
+        myepics.push(etiketje)
+      end
+    end
+  rescue
+  end
+end
+
+epicstring = ""
+myepics.uniq.sort.each do |epique|
+  epicstring = epicstring + epique + " "
+end
+puts epicstring
