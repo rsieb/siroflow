6b88d620937fc21c471009afbcff174a78a7364f
Roland Siebelink <git@rolandsiebelink.com>
2014-01-28 17:35:11 -0800

    Cleaned up chaseidle a bit and added a safeguard for double execution with a lockfile

--- chaseidle-launchdaemon.sh
+++ chaseidle-launchdaemon.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/bin/bash /Users/rs/.rbenv_include.sh ; ruby $HOME/rt/chaseidle.rb >> /Users/rs/Library/Logs/com.rolandsiebelink.chaseidle.log 2>&1
+source /Users/rs/.rbenv_include.sh
+ruby $HOME/rt/chaseidle.rb >> /Users/rs/Library/Logs/com.rolandsiebelink.chaseidle.log 2>&1
--- chaseidle.rb
+++ chaseidle.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 #!/usr/bin/env ruby -KU
 # encoding: UTF-8
 
@@ -9,54 +7,38 @@ require '/Users/rs/rt/Log.rb'
 require '/Users/rs/rt/Activity.rb'
 require '/Users/rs/rt/Pomodoro.rb'
 require 'Logger'
-
+require 'active_support/core_ext/numeric/time'
+LOCKFILE = '/tmp/chaseidle.lock'
 LOGGER = Logger.new(STDERR)
 LOGGER.level = Logger::DEBUG
-
-
-# 2012-09-09 this is double with the system call below. why?
-# TODOLIST = `/Users/rs/rt/starredtasks.rb`
-# unless TODOLIST.size < 3 # means no real output from process
-# #  puts "TODOLIST exists"
-# puts TODOLIST
-#   f = File.open('/Users/rs/Library/Application Support/Notational Data/starredtasks.txt', "w")
-#   f.write("#{TODOLIST}")
-#   puts "Written to starredtasks.txt"
-#   f.close
-# else
-#   puts "TODOLIST is empty"
-# end
-#system("/usr/bin/env ruby /Users/rs/Dropbox/Library/Scripts/Routinetracker/starredtasks.rb > /Users/rs/Dropbox/Library/Scripts/geektool/starredtasks.txt ")
-
-
-LOGFILE = "/tmp/routinetracker.log"
+LOGFILE = '/tmp/routinetracker.log'
 LOGGER.debug "LOGFILE = #{LOGFILE}"
 
-if ( IO.readlines(LOGFILE).first.match("PREZMODE").to_s.size == 0 )
-  LOGGER.debug "PREZMODE not active"
-  module RoutineTracker
-    class Main < Activity
-      LOGGER.debug "actual = #{actual}"
-      if idle?(actual)
-        LOGGER.debug "idle = true"
-        LOGGER.info "Chaseup"
-        LOGGER.debug "targetfile = #{targetfile}"
-        Terminal.chaseup(targetfile)
-      else
-        #system("/usr/bin/osascript /Users/rs/Dropbox/Library/Scripts/Applications/Pomodoro/distracting_apps.scpt")
-        LOGGER.debug "idle = false"
-        LOGGER.info "Remind"
-        LOGGER.debug "reminding of  = #{actual}"
-        Terminal.remind(actual)
+if File.mtime(LOCKFILE) > 60.seconds.ago
+  LOGGER.warn 'ChaseIdle already active. Abort.'
+else
+  File.write(LOCKFILE, '')
+
+  if IO.readlines(LOGFILE).first.match('PREZMODE').to_s.size == 0
+    LOGGER.debug 'PREZMODE not active'
+    module RoutineTracker
+      # Making sure this script somehow inherits from Activity. God so ugly.
+      class Main < Activity
+        LOGGER.debug "actual = #{actual}"
+        if idle?(actual)
+          LOGGER.debug 'idle = true'
+          LOGGER.info 'Chaseup'
+          LOGGER.debug "targetfile = #{targetfile}"
+          Terminal.chaseup(targetfile)
+        else
+          LOGGER.debug 'idle = false'
+          LOGGER.info 'Remind'
+          LOGGER.debug "reminding of  = #{actual}"
+          Terminal.remind(actual)
+        end
       end
     end
   end
-else
-  # do nothing for now
-  # if ( Time.now() - File.mtime(LOGFILE) > 4000 )
-  #   f = File.open(LOGFILE, "w+")
-  #   f.write("IDLE")
-  #   f.close
-  #   system("/usr/bin/osascript -f '/Users/rs/Dropbox/Library/Scripts/Mode work.scpt' ")
-  # end
+
+  File.delete(LOCKFILE)
 end
