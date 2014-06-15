4be88687193e008d6c8fbeb55b23a7ed9855b5e3
Roland Siebelink <git@rolandsiebelink.com>
2014-06-15 13:38:13 -0700

    Cleaned up Todyn

---
+++ calendar_events_today.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+source '/Users/rs/.bash_include_rs'
+
+/usr/local/bin/icalBuddy                                \
+	--noPropertyNames                                   \
+	--sectionSeparator ""                               \
+	--bullet "" 										\
+	--excludeEndDates 									\
+	--timeFormat "%Hh%M" 								\
+	--dateFormat "%a %d %b"								\
+	--propertySeparators "/ /" 							\
+	--excludeEventProps "url,notes,location,attendees"  \
+	--excludeAllDayEvents                               \
+	--noCalendarNames									\
+	--propertyOrder "datetime,title"					\
+	--includeCals "roland@rocketfuelinc.com,roland@siebelink.org,Tripit,Routines,Facebook Events,DSE Runners" \
+	--includeOnlyEventsFromNowOn						\
+	eventsToday | grep -v "»" | grep -v "√" | head -5
+
+# /usr/local/bin/icalBuddy                                \
+# 	--noPropertyNames                                   \
+# 	--sectionSeparator ""                               \
+# 	--bullet "" 										\
+# 	--excludeEndDates 									\
+# 	--timeFormat "%Hh%M" 								\
+# 	--dateFormat "%a %d %b"								\
+# 	--propertySeparators "/ /" 							\
+# 	--excludeEventProps "url,notes,location,attendees"  \
+# 	--noCalendarNames									\
+# 	--propertyOrder "datetime,title"					\
+# 	--includeCals "Big,DSE Runners" \
+# 	--includeOnlyEventsFromNowOn						\
+# 	--limitItems 1                                      \
+# 	eventsToday+7 | grep -v "Folding"
+
+#--excludeAllDayEvents								\
