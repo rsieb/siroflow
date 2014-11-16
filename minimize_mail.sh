5dff52daf1fe3d36f0073dc7365ceac39c25bbb2
Roland Siebelink <git@rolandsiebelink.com>
2014-11-16 04:36:54 -0800

    Manual override in Beeminder, minimize mail local changes

--- minimize_mail.sh
+++ minimize_mail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ /Users/rs/Desktop/allowmail -ot /tmp/pomodorodone.log ]
+if [ /Users/rs/Desktop/allowmail -ot /tmp/com.rolandsiebelink.beeminder-autoupdate.out ]
 	then
 	for MAILAPP in "Sparrow" "Mailplane 3" "Mail"
 		do
