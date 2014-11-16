66876e5c78cd1756a4871fa5f33cbf92320d0ee0
Roland Siebelink <git@rolandsiebelink.com>
2014-11-16 04:38:00 -0800

    Taking minimize_mail.sh out because it disrupts the email databases

--- minimize_mail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ /Users/rs/Desktop/allowmail -ot /tmp/com.rolandsiebelink.beeminder-autoupdate.out ]
-	then
-	for MAILAPP in "Sparrow" "Mailplane 3" "Mail"
-		do
-		killall $MAILAPP
-	done
-fi
